a11c6f8abf9d1c420bbbbbe0f877adfd8de66740
Emmanuel Baling <ej.baling@gmail.com>
2016-08-24 20:51:59 +1200

    Link EF6 to SQLite

--- AgeRanger/Repository/AgeRangerContext.cs
+++ AgeRanger/Repository/AgeRangerContext.cs
@@ -1,8 +1,23 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using AgeRanger.Models;
 
 namespace AgeRanger.Repository
 {
     public class AgeRangerContext : DbContext
     {
+        public AgeRangerContext()
+        {
+            // Turn off the Migrations, (NOT a code first Db)
+            Database.SetInitializer<AgeRangerContext>(null);    
+        }
+
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Database does not pluralize table names
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
