ddd142145e3c25a22a8d6f741908a3c9d1e508a3
Emmanuel <emmanuel@unleashedsoftware.com>
2016-08-26 10:07:36 +1200

    Unit test given use case

---
+++ AgeRanger.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using AgeRanger.Models;
+using AgeRanger.Service;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
+
+namespace AgeRanger.Tests
+{
+    [TestFixture]
+    public class AgeRangerTests
+    {
+        private Mock<IGetItemsUseCase> _getItemsUseCaseMock;
+
+        [Test]
+        public void GivenSearchTerm_WhenISearch_TheReturnFilteredResult()
+        {
+            const string searchTerm = "Will";
+            _getItemsUseCaseMock = new Mock<IGetItemsUseCase>();
+            _getItemsUseCaseMock.Setup(x => x.Execute(It.IsAny<string>())).Returns(new PersonAgeGroup[]
+            {
+                new PersonAgeGroup
+                {
+                    Id = 1,
+                    FirstName = "Will",
+                    LastName = "Smith",
+                    Age = 50
+                },
+                new PersonAgeGroup
+                {
+                    Id = 1,
+                    FirstName = "Anne",
+                    LastName = "Smith",
+                    Age = 51
+                }
+            }.AsQueryable());
+
+            var useCase = new GetItemUseCase();
+            var list = useCase.Execute(searchTerm).ToArray();
+            Assert.That(list.Count(), Is.EqualTo(1));
+        }
+    }
+}
